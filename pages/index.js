b78ad7271822f3924c120514e4781ec093633b98
Eduardo balan Hubner <eduardobhubner@gmail.com>
2021-07-13 13:49:05 -0300

    primeira aula react com a alura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,73 @@
-import styled from 'styled-components'
+// import styled from 'styled-components'
+import MainGrid from '../pages/src/components/MainGrid'
+import Box from '../pages/src/components/Box'
+import {AlurakutMenu} from '../pages/src/lib/AlurakutCommons'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+const gitHubUser = 'eduardobhubner';
+
+
+
+function ProfileSlideBar(propriedades) {
+  console.log(propriedades)
+  return (
+    <Box>
+      <img src={`https://www.github.com/${propriedades.gitHubUser}.png`} style={{ borderRadiun: "8px" }} />
+    </Box>
+  )
+}
+
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+
+const comunidades = [
+  'juunegreiros',
+  'omariosouto',
+  'peas',
+  'rafaballerini',
+  'marcobrunodev',
+  'felipefialho',
+];
+
+const pessoasFavoritas = [
+  'juunegreiros',
+  'omariosouto',
+  'peas',
+  'rafaballerini',
+  'marcobrunodev',
+  'felipefialho',
+];
+
+  return (
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSlideBar gitHubUser={gitHubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>Bem vindo</Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: "profileRelationsArea" }}>
+          <Box>
+            Pessoas da comunidade
+
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                  </li>
+                )
+              })
+              }
+            </ul>
+
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
